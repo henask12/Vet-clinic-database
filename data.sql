5d56946e9d326fbfe311fabc340f11efce550da8
Henok <henokmekonnen5@gmail.com>
2023-08-25 00:37:31 +0300

    Insert data into species table

--- data.sql
+++ data.sql
@@ -31,3 +31,9 @@ VALUES
     ('Melody Pond', 77),
     ('Dean Winchester', 14),
     ('Jodie Whittaker', 38);
+
+-- Insert data into species table
+INSERT INTO species (name)
+VALUES
+    ('Pokemon'),
+    ('Digimon');
