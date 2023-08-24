0b2beb0fa5960e399b4acde68446eb0208657199
Henok <henokmekonnen5@gmail.com>
2023-08-24 08:42:54 +0300

    database and tables created with data populated

--- schema.sql
+++ schema.sql
@@ -3,3 +3,24 @@
 CREATE TABLE animals (
     name varchar(100)
 );
+
+--vet_clinic database CREATE
+CREATE DATABASE vet_clinic
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+-- animals table CREATE
+    	CREATE TABLE animals (
+    id serial PRIMARY KEY,
+    name varchar(255),
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal
+);
