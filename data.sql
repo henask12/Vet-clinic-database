44fc4a4a9a0b068e70518092ae63c75e9a38e700
Henok <henokmekonnen5@gmail.com>
2023-08-25 00:36:50 +0300

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -21,3 +21,13 @@ VALUES
   ('Boarmon', '2005-06-07', 7, true, 20.4, NULL),
   ('Blossom', '1998-10-13', 3, true, 17, NULL),
   ('Ditto', '2022-05-14', 4, true, 22, NULL);
+
+-- Insert data into owners table
+INSERT INTO owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
