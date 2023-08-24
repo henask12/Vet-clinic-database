37bbb22c7af222da9c5b9080c5925fa24be81ea0
Henok <henokmekonnen5@gmail.com>
2023-08-25 01:06:16 +0300

    What animals belong to Melody Pond?

--- queries.sql
+++ queries.sql
@@ -114,8 +114,11 @@ FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
 
-
-
+-- What animals belong to Melody Pond?
+SELECT animals.name
+FROM animals
+JOIN owners ON animals.owner_id = owners.id
+WHERE owners.full_name = 'Melody Pond';
 
 
 
