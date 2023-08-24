debee9826f619300d2368e877391bf4dc02a8154
Henok <henokmekonnen5@gmail.com>
2023-08-24 23:49:23 +0300

    update queries

--- queries.sql
+++ queries.sql
@@ -19,6 +19,103 @@ SELECT * FROM animals WHERE name <> 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+-- Begin transaction
+BEGIN;
+
+-- Update the species to 'unspecified'
+UPDATE animals SET species = 'unspecified';
+
+-- Verify the change
+SELECT * FROM animals;
+
+-- Rollback the changes
+ROLLBACK;
+
+-- Verify that species columns went back to the state before the transaction
+SELECT * FROM animals;
+
+
+
+
+-- Begin transaction
+BEGIN;
+
+-- Update animals with name ending in 'mon'
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+
+-- Update animals with no species already set
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+
+-- Verify changes
+SELECT * FROM animals;
+
+-- Commit the transaction
+COMMIT;
+
+-- Verify changes persist after commit
+SELECT * FROM animals;
+
+-- Begin transaction
+BEGIN;
+
+-- Delete all records in the animals table
+DELETE FROM animals;
+
+-- Verify that records were deleted
+SELECT * FROM animals;
+
+-- Rollback the transaction
+ROLLBACK;
+
+-- Verify that records still exist
+SELECT * FROM animals;
+
+-- Begin transaction
+BEGIN;
+
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint
+SAVEPOINT my_savepoint;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO my_savepoint;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+
+-- Commit the transaction
+COMMIT;
+
+-- queries for the questions
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+GROUP BY neutered
+ORDER BY avg_escape_attempts DESC
+LIMIT 1;
+
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
+FROM animals
+GROUP BY species;
+
+
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
+
+
 
 
 
